3f596c558cd0402e013c1cc7e6c200820b50aba9
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-23 14:24:42 +0100

    add js solution

--- js/script.js
+++ js/script.js
@@ -26,4 +26,16 @@ for (let i = 1; i < 100; i++) {
      * In caso contrario chiediamo se fosse divisibile per 5
      */
 
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }
+    else if(i % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(i % 5 === 0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
 }
